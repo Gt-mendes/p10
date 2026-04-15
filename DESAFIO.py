4aa6826dd463149da487a8affdeef2cd0be13514
Gt-mendes <gabrieltiburcio143@gmail.com>
2026-04-15 17:04:02 -0300

    Add files via upload

---
+++ DESAFIO.py
@@ -0,0 +1,9 @@
+
+animais = ["Cachorro", "Gato", "Peixe"]
+
+print(f"Lista de animais: {animais}")
+print(f"Primeiro animal: {animais[0]}")
+print(f"Último animal: {animais[-1]}")
+
+animais.append("Hamster")
+print(f"Lista atualizada: {animais}")
