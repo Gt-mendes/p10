34a258d62e85f33f93d3616b9eb6893b750dc8ea
Gt-mendes <gabrieltiburcio143@gmail.com>
2026-04-15 17:01:33 -0300

    Delete dicionario_criar.py

--- dicionario_criar.py
+++
@@ -1,9 +0,0 @@
-# Criando um dicionario com informacoes de uma pessoa
-pessoa = {
-    "Nome": "Joao",
-    "Idade": 25,
-    "Cidade": "Sao Paulo"
-}
-
-print(pessoa)
-print(f"Tipo: {type(pessoa)}")
