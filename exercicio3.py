52a7f13f12f5468c8cd48655d6dc20814cc83f26
Gt-mendes <gabrieltiburcio143@gmail.com>
2026-04-15 17:02:09 -0300

    Delete exercicio3.py

--- exercicio3.py
+++
@@ -1,8 +0,0 @@
-# Exercicio 3: Dias da semana (tupla)
-dias = ("Domingo", "Segunda", "Terca", "Quarta", "Quinta", "Sexta", "Sabado")
-
-print(f"Todos os dias: {dias}")
-print(f"Primeiro dia: {dias[0]}")
-print(f"Ultimo dia: {dias[-1]}")
-print(f"Dia mais legal: {dias[5]}")
-print(f"Dia mais paia: {dias[1]}")
